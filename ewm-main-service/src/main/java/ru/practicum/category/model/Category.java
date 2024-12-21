479e1a4805b851cbde4a56fcc3b59f047b11e189
el3shka <harv3st3r666@gmail.com>
2024-12-22 05:23:27 +0600

    main_Svc2

---
+++ ewm-main-service/src/main/java/ru/practicum/category/model/Category.java
@@ -0,0 +1,28 @@
+package ru.practicum.category.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
