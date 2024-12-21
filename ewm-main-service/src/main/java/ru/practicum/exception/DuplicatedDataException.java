f46b4224881e6fa5a26651c2097960a4a93fd7c1
el3shka <harv3st3r666@gmail.com>
2024-12-22 05:19:48 +0600

    main_Svc2

--- ewm-main-service/src/main/java/ru/practicum/exception/DuplicatedDataException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.exception;
-
-public class DuplicatedDataException extends RuntimeException {
-    public DuplicatedDataException(final String message) {
-        super(message);
-    }
-}
